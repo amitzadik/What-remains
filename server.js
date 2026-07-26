6c05c7d3f9ee6dda7661d325041915afa8e8e53d
Amit Tzadik <amitzadik@gmail.com>
2026-07-26 19:29:34 +0300

    Restore landing page as deployment homepage

--- server.js
+++ server.js
@@ -10,7 +10,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.static('.'));
-app.use(express.static('public'));
 
 app.get('/', (req, res) => {
   res.sendFile(process.cwd() + '/index.html');
